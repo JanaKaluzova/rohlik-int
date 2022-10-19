4b566a1ff2334d71b5c9a5006f9f99b595bb5136
Jana Kalužová <jani.kaluzova@gmail.com>
2022-10-19 10:02:06 +0200

    font changed

--- components/StoreItem.tsx
+++ components/StoreItem.tsx
@@ -11,8 +11,8 @@ export const StoreItem: React.FC<StoreItemProps> = ({ id, name, image, price }) 
       <Card.Img className="p-2" src={image} height="100px" style={{ objectFit: 'contain' }} />
       <Card.Body className="d-flex flex-column">
         <div className="d-flex flex-column justify-content-between align-items-center mb-auto">
-          <span className="fs-20 mt-4">{name}</span>
-          <span className="text-muted d-flex flex-row mt-auto mb-2">{formatCurrency(price.full)}</span>
+          <span className="fs-20 mt-4 text-center text-muted">{name}</span>
+          <span className="d-flex flex-row mt-auto mb-2 fs-5">{formatCurrency(price.full)}</span>
         </div>
         <div className="mt-auto">
           {quantity === 0 ? (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Head from 'next/head'
-import { Header } from '../components/Header'
 import { ShoppingCartProvider } from '../context/ShoppingCartContext'
+import './app.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Rohlik shopping cart</title>
         <meta name="description" content="Rohlik inteview app" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles&display=swap" rel="stylesheet" />
       </Head>
       <ShoppingCartProvider>
         <Component {...pageProps} />
